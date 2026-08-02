e50827ab8a68db9f3e5cb9a9c98f9de4abffd9dc
mrbathwater <106950691+mrbathwater@users.noreply.github.com>
2026-08-02 03:02:37 -0400

    Update version numbers for AVB_ROOT, CUSTOTA, and OEMUnlockOnBoot

--- rooted-ota.sh
+++ rooted-ota.sh
@@ -67,15 +67,15 @@ NO_COLOR=${NO_COLOR:-''}
 OTA_BASE_URL="https://releases.grapheneos.org"
 
 # renovate: datasource=github-releases packageName=chenxiaolong/avbroot versioning=semver
-AVB_ROOT_VERSION=3.31.0
+AVB_ROOT_VERSION=3.32.2
 # renovate: datasource=github-releases packageName=chenxiaolong/Custota versioning=semver-coerced
-CUSTOTA_VERSION=6.1
+CUSTOTA_VERSION=6.3
 # renovate: datasource=git-refs packageName=https://github.com/chenxiaolong/my-avbroot-setup currentValue=master
 PATCH_PY_COMMIT=84139189c8cbe244a676582a3b3517f31fabc421
 # renovate: datasource=docker packageName=python
 PYTHON_VERSION=3.14.2-alpine
 # renovate: datasource=github-releases packageName=chenxiaolong/OEMUnlockOnBoot versioning=semver-coerced
-OEMUNLOCKONBOOT_VERSION=1.3
+OEMUNLOCKONBOOT_VERSION=1.4
 # renovate: datasource=github-releases packageName=chenxiaolong/afsr versioning=semver
 AFSR_VERSION=1.0.4
 
